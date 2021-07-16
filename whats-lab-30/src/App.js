7eb079065b4e0fb7d51f21476b99b4ba30a02e8f
YaraNascimento <yara.tacy.barbosa.29@gmail.com>
2021-07-16 15:27:11 -0300

    Adicinamos os inputs

---
+++ whats-lab-30/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+import "./components/Components"
+import styled from "styled-components";
+
+
+
+function App() {
+  
+  return <form>
+    <input type="text"
+    nome="Usuario"
+    placeholder="Usuário"
+    />
+    <input type="text"
+    nome="Mensagem"
+    placeholder="Mensagem"
+    />
+    <button type="submit">Enviar</button>
+  </form>
+}
+
+export default App;
